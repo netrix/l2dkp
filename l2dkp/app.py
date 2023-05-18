2767d698f6fa87026b90bf94284ba8f4d0174cc6
netrix <netrix89@gmail.com>
2023-05-18 22:03:28 +0200

    feat: Add pydantic for request and response model and new POST /raid endpoint.

--- l2dkp/app.py
+++ l2dkp/app.py
@@ -1,5 +1,8 @@
 import pathlib
 from flask import Flask, request, make_response
+from pydantic import BaseModel
+from flask_pydantic import validate
+
 
 
 app = Flask(__name__)
@@ -40,31 +43,54 @@ def catch_all(path):
         return app.send_static_file(path + ".html")
 
 
+
+class RaidInfo(BaseModel):
+    name: str
+    date: str
+    people: list[str]
+    drops: list[str]
+
+
+class RaidsResponse(BaseModel):
+    raids: list[RaidInfo]
+
+
 @app.route('/api/raids')
-def get_raids():
-    return [
-        {
-            "name": "Baium",
-            "date": '2018-04-04T16:00:00.000Z',
-            "people": [
-            "DarkNetrix",
-            "NetrixDSS",
-            ],
-            "drops": [
-            "Majestic Robe",
-            "Majestic Gauntlets"
-            ]
-        },
-        {
-            "name": "Valakas",
-            "date": '2018-04-04T16:00:00.000Z',
-            "people": [
-            "Sheldon",
-            "NetrixDSS",
-            ],
-            "drops": [
-            "Nightmare Robe",
-            "Nightmare Gauntlets"
-            ]
-        }
-    ]
+@validate()
+def get_raids() -> RaidsResponse:
+    return RaidsResponse.parse_obj({
+        "raids": [
+            {
+                "name": "Baium",
+                "date": '2018-04-04T16:00:00.000Z',
+                "people": [
+                "DarkNetrix",
+                "NetrixDSS",
+                ],
+                "drops": [
+                "Majestic Robe",
+                "Majestic Gauntlets"
+                ]
+            },
+            {
+                "name": "Valakas",
+                "date": '2018-04-04T16:00:00.000Z',
+                "people": [
+                "Sheldon",
+                "NetrixDSS",
+                ],
+                "drops": [
+                "Nightmare Robe",
+                "Nightmare Gauntlets"
+                ]
+            }
+        ]
+    })
+
+
+
+@app.route('/api/raids', methods=["POST"])
+@validate()
+def add_raid(body: RaidInfo) -> None:
+    print(body)
+    return {}
