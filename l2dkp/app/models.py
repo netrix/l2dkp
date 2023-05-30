3567e27ebb23182641602a962b2f7320423a9b4a
netrix <netrix89@gmail.com>
2023-05-30 23:35:41 +0200

    fix: Change length of password field to 255 (hash did not fit in 64).

--- l2dkp/app/models.py
+++ l2dkp/app/models.py
@@ -79,4 +79,4 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
